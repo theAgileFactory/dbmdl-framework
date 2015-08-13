176e6e2f6bca7b6c61ea9129cb1c9068a7bba449
jkohlertaf <johann.kohler@the-agile-factory.com>
2015-08-13 15:10:40 +0200

    Add a isDisplay flag for API keys (refs #1557)

---
+++ src/main/resources/repo/scripts/20150813150000_api_registration.sql
@@ -0,0 +1,13 @@
+--add a is_displayed flag for the api key
+
+ALTER TABLE `api_registration` ADD COLUMN `is_displayed` TINYINT(1) NOT NULL DEFAULT 1  AFTER `description` ;
+
+--//@UNDO
+
+ALTER TABLE `api_registration` DROP COLUMN `is_displayed` ;
+
+
+
+
+
+
