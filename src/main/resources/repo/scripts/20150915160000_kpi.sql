67545581a31e86f029f5c33da5ae9ca3428e72e4
jkohlertaf <johann.kohler@the-agile-factory.com>
2015-09-15 13:36:27 +0200

    Add release burndown KPI (refs #1628)

---
+++ src/main/resources/repo/scripts/20150915160000_kpi.sql
@@ -0,0 +1,11 @@
+--add is trend displayed flag in value definition
+
+ALTER TABLE `kpi_value_definition` ADD COLUMN `is_trend_displayed` TINYINT(1) NOT NULL DEFAULT 1  AFTER `computation_js_code` ;
+
+UPDATE `kpi_value_definition` SET `is_trend_displayed` = '0' WHERE `name` LIKE 'kpi.%.additional1.name' OR `name` like 'kpi.%.additional2.name'
+
+--//@UNDO
+
+ALTER TABLE `kpi_value_definition` DROP COLUMN `is_trend_displayed` ;
+
+
