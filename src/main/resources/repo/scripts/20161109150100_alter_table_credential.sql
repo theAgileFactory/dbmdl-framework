774b3d51a698a93b591da1026cce0cd0ebd793f5
Guillaume Petit <guillaume.petit30@gmail.com>
2017-02-14 10:59:37 +0100

    #82 Add last login date in users list

--- src/main/resources/repo/scripts/20161109150100_alter_table_credential.sql
+++ src/main/resources/repo/scripts/20161109150100_alter_table_credential.sql
@@ -1,2 +1,11 @@
+-- // add first and last planned lcmi in pe
+-- Migration SQL that makes the change goes here.
+
 ALTER TABLE `credential`
 	ADD COLUMN `last_login_date` DATETIME NULL DEFAULT NULL AFTER `last_update`;
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+
+ALTER TABLE `credential`
+DROP COLUMN `last_login_date`;
