7777382362c560f9c3e94a8a68e76fa83147d08c
Guillaume Petit <guillaume.petit30@gmail.com>
2017-06-15 11:26:46 +0200

    chmod +x

--- src/main/resources/scripts/run.sh
+++ src/main/resources/scripts/run.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
-
 cd `dirname $0`
-
-chmod u+x *.sh
-
-echo "Execute database migration if needed"
-#mvn -f migration-pom.xml migration:up -Dmigration.env=deploy
-./migrate.sh up --path=../repo --env=deploy
-
-echo "End of execution"
+./migrate.sh $1 "${@:3}" --path=../repo --env=$2 || exit -1
