efe4846551962ff0bf503eeecbb39a1cbdb528d2
Guillaume Petit <guillaume.petit30@gmail.com>
2017-10-04 10:14:37 +0200

    #116 Rollback swiss locale (that sucked)

--- src/main/resources/repo/scripts/20170828121032_v16-0-0_Change_french_locale.sql
+++
@@ -1,9 +0,0 @@
--- // 16.0.0_Change_french_locale
--- Migration SQL that makes the change goes here.
-
-ALTER TABLE i18n_messages MODIFY COLUMN language VARCHAR(6) NOT NULL;
-
--- //@UNDO
--- SQL to undo the change goes here.
-
-ALTER TABLE i18n_messages MODIFY COLUMN language VARCHAR(2) NOT NULL;
